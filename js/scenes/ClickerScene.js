2e64625619e987b424da1013c24353c935e845ff
aah haa <lightningmcqueenwow@gmail.com>
2026-08-10 14:47:12 -0500

    background changes

--- js/scenes/ClickerScene.js
+++ js/scenes/ClickerScene.js
@@ -10,6 +10,13 @@ export class ClickerScene {
         this.screen = document.createElement("div");
         this.screen.classList.add("clicker-scene");
 
+        this.world = document.createElement("div");
+        this.world.classList.add("game-world");
+
+        this.background = document.createElement("img");
+        this.background.src = "assets/images/clicker-background.png";
+        this.background.classList.add("background");
+
         this.chester = document.createElement("img");
         this.chester.src = "assets/images/chester.png";
         this.chester.classList.add("rat");
@@ -21,10 +28,12 @@ export class ClickerScene {
         this.cheeseCounter = document.createElement("div");
         this.cheeseCounter.classList.add("cheese-counter");
 
+        this.world.appendChild(this.background);
+        this.world.appendChild(this.chester);
+        this.world.appendChild(this.extortButton);
+        this.world.appendChild(this.cheeseCounter);
 
-        this.screen.appendChild(this.chester);
-        this.screen.appendChild(this.extortButton);
-        this.screen.appendChild(this.cheeseCounter);
+        this.screen.appendChild(this.world);
 
         container.appendChild(this.screen);
 
@@ -35,12 +44,12 @@ export class ClickerScene {
 
     extort = () => {
         this.gameState.cheese += 1;
-
         this.updateCheeseDisplay();
     }
 
     updateCheeseDisplay() {
-        this.cheeseCounter.textContent = `Cheese: ${this.gameState.cheese}`;
+        this.cheeseCounter.textContent =
+            `Cheese: ${this.gameState.cheese}`;
     }
 
     exit() {
